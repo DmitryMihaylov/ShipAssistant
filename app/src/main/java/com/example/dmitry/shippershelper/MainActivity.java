4befee58a73483e13d82959acf8ef1f3cd8d88bc
DmitryMihailov <dimamihailov1108@gmail.com>
2018-05-06 19:36:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dmitry/shippershelper/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.dmitry.shippershelper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+    private Button buttonStart;
+    private Button buttonAbout;
+    private Button buttonExit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonAbout = findViewById(R.id.buttonAbout);
+        buttonExit = findViewById(R.id.buttonExit);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(intent);
+            }
+        });
+        buttonAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+        buttonExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/dmitry/shippershelper/VariantActivity.java
@@ -0,0 +1,4 @@
+package com.example.dmitry.shippershelper;
+
+public class VariantActivity {
+}
